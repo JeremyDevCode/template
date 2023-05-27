a3d1dfd511008e73c6b9f2f1150e72d51e9b84b5
Unai Mengual <git@owln.ai>
2023-05-27 10:29:53 +0200

    fix: improve overall styling

--- src/common/Decorator.tsx
+++ src/common/Decorator.tsx
@@ -9,7 +9,7 @@ const Decorator = ({ icon }: DecoratorProps) => {
     <div className="flex items-center gap-16">
       <div>{icon}</div>
       <div className="hidden sm:block">{icon}</div>
-      <div className="hidden md:block">{icon}</div>
+      <div className="hidden lg:block">{icon}</div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,18 @@ import Button from "@/common/Button";
 import CurriculumIcon from "@/assets/icons/CurriculumIcon";
 import LinkedInIcon from "@/assets/icons/LinkedInIcon";
 import Paragraph from "@/common/Paragraph";
+import type { ReducerAction } from "react";
+import type React from "react";
 
 const Header = () => {
   return (
-    <header className="flex items-center h-full w-full flex-col gap-16 text-[#141414] dark:text-white">
-      <h1 className="font-bold text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-center md:pb-4 lg:pb-8">
-        <span className="text-transparent bg-clip-text bg-gradient-to-b from-[#6CEF79] to-[#1DD7F0]">
+    <header className="flex items-center h-full w-full flex-col gap-12 md:gap-16 text-[#141414] dark:text-white">
+      <h1 className="font-bold mt-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-center md:pb-4 lg:pb-8" style={{'--stagger':1} as React.CSSProperties} data-animate>
+        <span className="text-transparent bg-clip-text [background-image:linear-gradient(180deg,_#6CEF79_7.35%,#1DD7F0_93.27%,#1DD7F0_93.28%)]">
           From pixels
         </span>
         <br />
-        <span className="text-transparent bg-clip-text bg-gradient-to-b from-[#6CEF79] to-[#1DD7F0]">
+        <span className="text-transparent bg-clip-text [background-image:linear-gradient(180deg,_#6CEF79_7.35%,#1DD7F0_93.27%,#1DD7F0_93.28%)]">
           to magic
         </span>
       </h1>
@@ -19,7 +21,7 @@ const Header = () => {
         Soy un Ingeniero de Sistemas especializado en diseño de bases de datos y
         programación SQL
       </Paragraph>
-      <div className="flex justify-center gap-4 sm:gap-16">
+      <div className="flex justify-center gap-4 sm:gap-16" style={{ '--stagger': 3 } as React.CSSProperties} data-animate>
         <Button
           content="Linkedin"
           icon={<LinkedInIcon className="w-6 h-6 sm:w-8 sm:h-8" />}
