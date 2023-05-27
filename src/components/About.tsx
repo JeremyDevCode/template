f6a8ceeaae94e8bf471356750d9b33fc2a710d11
Christian Villegas <chrisvill.2312@gmail.com>
2023-05-27 04:04:52 -0300

    FIX: Style on About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,11 +11,11 @@ function About() {
         Sobre mi
       </TitleSeparator>
       <div className="flex flex-col w-3/4 lg:w-2/3 xl:w-1/2 items-center gap-6 sm:gap-8 md:gap-10 lg:gap-12">
-        <p className="text-xl sm:text-2xl text-[#C7CCD7] font-medium ">
+        <p className="text-xl sm:text-2xl lg:text-3xl text-[#C7CCD7] font-medium ">
           Nací en Colombia y tengo una titulación en Ingeniería de Sistemas,
           complementada con un postgrado en Power BI y Análisis de Información.
         </p>
-        <p className="text-xl sm:text-2xl text-[#C7CCD7] font-medium ">
+        <p className="text-xl sm:text-2xl lg:text-3xl text-[#C7CCD7] font-medium ">
           Actualmente trabajo en Afordin, y en mi tiempo libre me dedico a
           responder las dudas de Comuafor y poner enlaces a recursos guapos
           (como yo ).
